9c210c1532e92716b86916e7c8999cd03a2450f0
karbivskyi <karbivskij@gmail.com>
2025-07-26 01:42:12 +0300

    added create nft function

--- create_nft.ts
+++ create_nft.ts
@@ -1,67 +1,101 @@
-import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
-import { keypairIdentity, percentAmount } from "@metaplex-foundation/umi";
+import {
+  createNft,
+  findMetadataPda,
+  mplTokenMetadata,
+  verifyCollectionV1,
+} from "@metaplex-foundation/mpl-token-metadata";
+import {
+  createGenericFile,
+  generateSigner,
+  keypairIdentity,
+  percentAmount,
+  publicKey as UMIPublicKey,
+} from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { clusterApiUrl } from "@solana/web3.js";
-import { getKeypairFromFile } from "@solana-developers/helpers";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile,
+} from "@solana-developers/helpers";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { promises as fs } from "fs";
+import * as path from "path";
+// create a new connection to Solana's devnet cluster
+const connection = new Connection(clusterApiUrl("devnet"));
 
-const umi = createUmi(clusterApiUrl("devnet"));
+// load keypair from local file system
+// assumes that the keypair is already generated using `solana-keygen new`
+const user = await getKeypairFromFile("keypair.json");
+console.log("Loaded user:", user.publicKey.toBase58());
 
-//load keypair from local file system
-// See https://github.com/solana-developers/helpers?tab=readme-ov-file#get-a-keypair-from-a-keypair-file
-const localKeypair = await getKeypairFromFile('keypair.json');
+await airdropIfRequired(
+  connection,
+  user.publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.1 * LAMPORTS_PER_SOL,
+);
 
-// convert to Umi compatible keypair
-const umiKeypair = umi.eddsa.createKeypairFromSecretKey(localKeypair.secretKey);
+const umi = createUmi(connection);
 
-// load the MPL metadata program plugin and assign a signer to our umi instance
-umi.use(keypairIdentity(umiKeypair)).use(mplTokenMetadata());
+// convert to umi compatible keypair
+const umiKeypair = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
 
-let filePath = "lil_turtle.avif";
-const buffer = createGenericFile(Buffer, filePath, {
-    // chose the correct file MIME type https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
-    contentType: "image/avif",
-});
-const [image] = await umi.uploader.upload([file]);
+// load our plugins and signer
+umi
+  .use(keypairIdentity(umiKeypair))
+  .use(mplTokenMetadata())
+  .use(irysUploader());
 
-const uri = await umi.uploader.uploadJson({{
+  // Substitute in your collection NFT address from create_collection.ts
+  const collectionNftAddress = UMIPublicKey("GzguXDtqQ6hUQxTUSL8NMgnTFFgnFabLAM4rvyrK7UNj");
+
+  // example data and metadata for our NFT
+  const nftData = {
     name: "Lil Turtle",
     symbol: "LIL",
-    description: "A cute lil turtle NFT",
-    image: image,
-    external_url: "https://lil-turtle.framer.website/",
-    attributes: [
-        { trait_type: "Turtle", value: "Lil Turtle" },
-        { trait_type: "Size", value: "Little" },
-        {trait_type: "Hairstyle", value: "Mohawk" }
-    ],
-});
-
-const collectionMint = generateSigner(umi);
-
-await createNft(umi,{
-    mint: collectionMint,
-    name "Lil Turtle Collection",
-    uri: "https://lil-turtle.framer.website/",
+    description: "Small turtle with a big heart",
     sellerFeeBasisPoints: percentAmount(0),
-    isCollection: true,
-}).sendAndConfirm(umi);
+    imageFile: "lil_turtle.avif",
+  }
 
-const { signature, result } = await createNft(umi,{
+  const NFTImagePath = path.resolve(__dirname, "lil_turtle.avif");
+
+  const buffer = await fs.readFile(NFTImagePath);
+  let file = createGenericFile(buffer, NFTImagePath, {
+    contentType: "image/avif",
+  });
+
+  // upload image and get image uri
+  const [image] = await umi.uploader.upload([file]);
+  console.log("image uri:", image);
+
+  // upload offchain json using irys and get metadata uri
+  const uri = await umi.uploader.uploadJson({
+    name:"Lil Turtle",
+    symbol: "LIL",
+    description: "Small turtle with a big heart",
+    image,
+  });
+console.log("NFT offchain metadata URI:", uri);
+
+// generate mint keypair
+const mint = generateSigner(umi);
+
+//create and mint NFT
+await createNft(umi, {
     mint,
-    name: "Lil Turtle",
+    name: nftData.name,
+    symbol: nftData.symbol,
     uri,
     updateAuthority: umi.identity.publicKey,
-    sellerFreeBasisPoints: percentAmount(0),
-    collection: {key: collectionMint.publicKey, verified: false}
-}).sendAndConfirm(umi, {send :{commitment: "finalized"}});
+    sellerFeeBasisPoints: percentAmount(0),
+    collection: {
+        key: collectionNftAddress,
+        verified: false,
+    },
+}).sendAndConfirm(umi, { send: { commitment: "finalized" } });
 
-const metadata = findMetadataPda(umi, {mint: mintArgs.publicKey});
-
-await verifyCollectionV1(umi, {
-    metadata,
-    collectionMint,
-    authority: umi.identity,
-}).sendAndConfirm(umi);
-
-console.log("NFT created with signature:", signature);
+let explorererLink = getExplorerLink("address", mint.publicKey, "devnet");
+ 
+console.log(`💵 Token Mint : ${explorererLink}`);
